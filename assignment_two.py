f982532380f3c6394b90df6bfbe0d28ed3a30b8c
Thenea <thenea@iMac4.local>
2018-09-19 08:55:30 -0400

    I finished programing most of the project, and my chatbot seems to be really fun to talk with. I wish I could add more stuff into it, and create some funnier conversation

---
+++ assignment_two.py
@@ -0,0 +1,28 @@
+# Thenea_Sun
+# Sept. 19th
+# assignment_two
+# This is my second project, which I created a chat bot with codes, in order to get all the information that is
+# required in the rubric
+
+print("Hello! I am the artificial intelligence you are going to talk to in the next five minutes!")
+
+print("My name is San!")
+
+name_of_user = input("What is your name?")
+
+print(name_of_user, "sounds cute! I like this name start from now.")
+
+location_of_user = input("Where are you from then? By the way I am set to be from Maryland :P")
+
+print("From the knowledge I have,", location_of_user, "is a very enjoyable place.")
+
+favorite_number_of_user = input("Umm... Let's do some fun calculation. What is your favorite number?")
+
+difference_between_numbers = int(favorite_number_of_user) / 7
+
+print("I like all numbers, but I still got a favorite one. Now it it your turn to guess--my favorite number is",
+      difference_between_numbers, "times bigger than yours")
+
+print("Now I showed your my calculating skills, what about let me help you do something?")
+
+favorite_car_of_user =
