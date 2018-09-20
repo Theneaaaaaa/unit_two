08c02b847635ec3c75b61683da5a2b05807598a8
Thenea <thenea@iMac4.local>
2018-09-20 09:14:49 -0400

    Mathematical Operators

---
+++ d4_unit_two_warmups.py
@@ -0,0 +1,11 @@
+print(9 * 5)
+print(2 / 5)
+print(12 // 5)
+print(27 // 4)
+print(2 // 5)
+print(5 % 2)
+print(9 % 5)
+print(6 % 6)
+print(2 % 7)
+
+# // gives us integers division, % gives us modulus remainder
